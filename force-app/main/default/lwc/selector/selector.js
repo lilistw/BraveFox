090152450fea35cd003d132577a908cc80bde644
Lili Stoyanova <lili.st.work@gmail.com>
2024-12-06 20:36:07 +0200

    Create user LWC

--- force-app/main/default/lwc/selector/selector.js
+++ force-app/main/default/lwc/selector/selector.js
@@ -1,12 +1,4 @@
-// import { LightningElement } from 'lwc';
 
-// export default class Selector extends LightningElement {
-//     selectedProductId;
-
-//     handleProductSelected(evt) {
-//         this.selectedProductId = evt.detail;
-//     }
-// }
 import { LightningElement, wire } from 'lwc';
 import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
 import Id from '@salesforce/user/Id';
